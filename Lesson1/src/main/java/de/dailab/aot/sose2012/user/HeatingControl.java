3886ad9d28a120ebafb7d4e8f77e5abb9af98f0f
Marcelo Garlet Millani <marcelogmillani@gmail.com>
2014-05-01 17:56:32 +0200

    Added a class for the agent. Currently, it does nothing.

---
+++ Lesson1/src/main/java/de/dailab/aot/sose2012/user/HeatingControl.java
@@ -0,0 +1,16 @@
+package de.dailab.aot.sose2012.user;
+
+import de.dailab.jiactng.agentcore.AbstractAgentBean;
+
+public final class HeatingControl extends AbstractAgentBean {
+
+	/*@Override
+	public void doStart() throws Exception {
+		super.doStart();
+	}
+
+	@Override
+	public void doStop() throws Exception {
+	}*/
+
+}
